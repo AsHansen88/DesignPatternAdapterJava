c4d662c0de110c08af83437902021a8074e39738
AsHansen88 <Ande970s@stud.kea.dk>
2023-04-13 16:46:12 +0200

    Done

---
+++ src/com/company/Dogdog.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Dogdog implements Dog{
+
+  public void vov() {
+    System.out.println("vov");
+  }
+  public void play() {
+    System.out.println("Dog is playing");
+  }
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Dogdog dogdoing = new Dogdog();
+
+
+        dogdoing.play();
+        dogdoing.vov();
+
+
+
+
+
+    }
+}
